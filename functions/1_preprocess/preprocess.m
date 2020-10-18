a176d5f7caaed71243a916cc2c73443e3a68b1f7
Callum Tilbury <TLBCAL002@myuct.ac.za>
2020-10-18 14:46:52 +0200

    Added docstring for preprocess.m functionG

--- functions/1_preprocess/preprocess.m
+++ functions/1_preprocess/preprocess.m
@@ -1,4 +1,23 @@
-function dab_frame = preprocess(file_name,file_datatype,frame_count,file_offset, frame_offset, dab_mode, fs)
+function dab_frame = preprocess(file_name,file_datatype,frame_count, ...
+                                file_offset, frame_offset, dab_mode, fs)                        
+    % ---------------------------------------------------------------------    
+    % PREPROCESS: Extracts a DAB frame from a binary file
+    % ---------------------------------------------------------------------
+    % Usage:
+    %  Inputs
+    %   > file_name:        Name of Binary File
+    %   > file_datatype:    Format of data values, e.g. 'double'
+    %   > frame_count:      Number of frames to read into iq_data_raw
+    %   > file_offset:      Offset in binary file
+    %   > frame_offset:     Frame offset from first phase reference symbol
+    %   > dab_mode:         Structure containing constants for DAB mode
+    %   > fs:               Sampling frequency of IQ data
+    %
+    %  Outputs
+    %   < dab_frame:         IQ data of a single DAB frame
+    %
+    % ---------------------------------------------------------------------
+    
     %% IQ READ
     iq_data_raw = iq_read(file_name, file_datatype, frame_count, file_offset, dab_mode);
     
