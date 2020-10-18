e78a5d30ec66c533fa6a08792ec94f6f78e2f8c1
Callum Tilbury <TLBCAL002@myuct.ac.za>
2020-10-18 14:22:33 +0200

    Docstring for iq_resample

--- functions/1_preprocess/iq_resample.m
+++ functions/1_preprocess/iq_resample.m
@@ -1,11 +1,22 @@
 function iq_data = iq_resample(iq_data_raw, fs)
-    %IQ_RESAMPLE Summary of this function goes here
-    %   Detailed explanation goes here
+    % ---------------------------------------------------------------------    
+    % IQ_RESAMPLE: Resamples complex data if Fs != 2.048 MHz
+    % ---------------------------------------------------------------------
+    % Usage:
+    %  Inputs
+    %   > iq_data_raw:      Raw IQ data from iq_read function
+    %   > fs:               Sampling frequency of IQ data
+    %
+    %  Outputs
+    %   < iq_data_raw:      IQ data at correct Fs (2.048MHz) for COFDM
+    %
+    % ---------------------------------------------------------------------
     
-    % Resample to 2.048MHZ if not already
     if (fs ~= 2.048e6)
+        % Resample to 2.048MHZ, if not already
         iq_data = resample(iq_data_raw,2.048e6,fs);
     else
+        % Otherwise just return the input
         iq_data = iq_data_raw;
     end
     
