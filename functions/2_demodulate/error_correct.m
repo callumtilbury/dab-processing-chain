9c8de59866c130b9f9cc58778199f9ff24661a40
Callum Tilbury <TLBCAL002@myuct.ac.za>
2020-10-28 08:42:18 +0200

    Updated docstring for error_correct function, showing intended dimensions.

--- functions/2_demodulate/error_correct.m
+++ functions/2_demodulate/error_correct.m
@@ -2,8 +2,16 @@ function dab_data = error_correct(dab_data_snapped)
     % ---------------------------------------------------------------------    
     % ERROR_CORRECT: Apply error correction algorithm to DAB data
     % ---------------------------------------------------------------------
-    % 
-    %   TODO
+    % NB: NOT IMPLEMENTED IN THIS PROJECT
+    % ---------------------------------------------------------------------
+    % Proposed Usage:
+    %  Inputs
+    %   > dab_data_snapped:     Snapped DQPSK data
+    %                               [(dab_mode.L - 1) x dab_mode.K]
+    %
+    %  Outputs
+    %   < dab_data:             Error-corrected DQPSK data
+    %                               [(dab_mode.L - 1) x dab_mode.K]
     %
     % ---------------------------------------------------------------------    
     dab_data = dab_data_snapped;
