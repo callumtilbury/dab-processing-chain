35799167701e1129954c6c28dd0fc27801d887c8
Callum Tilbury <TLBCAL002@myuct.ac.za>
2020-11-11 10:15:44 +0200

    Updated comments.

--- plots/plot_dqpsk_snap.m
+++ plots/plot_dqpsk_snap.m
@@ -1,39 +1,45 @@
+% ---------------------------------------------------------------------    
+% PLOT_DQPSK_SNAP: Plots a demo of DQPSK snapping functionality
+% ---------------------------------------------------------------------
 % Constants
-mode1.K         = 1536;
-mode1.L         = 76;
-mode1.Tnull     = 2656;
-mode1.Tu        = 2048;
-mode1.Tg        = 504;
-mode1.Ts        = mode1.Tu + mode1.Tg;
-mode1.Tf        = mode1.Tnull + mode1.L * mode1.Ts;
-mode1.mask      = [257:1024,1026:1793];
+dab_mode = load_dab_constants(1);
 
+% File locations
+perfect.z = preprocess("/Volumes/clmtlbry-4/UCT/IV/S/EEE4022S/Data/DAB_data/Perfect Data/DAB_7A_188.928.bin", "double", 1, 1e6, 0, dab_mode, 2.048e6);
+raw.z = preprocess("/Volumes/clmtlbry-4/UCT/IV/S/EEE4022S/Data/DAB_data/raw data/DAB_data.bin", "short", 2, 0, 0, dab_mode, 2.5e6);
+rtl.z = preprocess("/Volumes/clmtlbry-4/UCT/IV/S/EEE4022S/Data/DAB_data/RTL-SDR/DAB.bin", "short", 2, 0, 0, dab_mode, 2.048e6);
 
-perfect.z = preprocess("/Volumes/clmtlbry-4/UCT/IV/S/EEE4022S/Data/DAB_data/Perfect Data/DAB_7A_188.928.bin", "double", 1, 1e6, 0, mode1, 2.048e6);
-raw.z = preprocess("/Volumes/clmtlbry-4/UCT/IV/S/EEE4022S/Data/DAB_data/raw data/DAB_data.bin", "short", 2, 0, 0, mode1, 2.5e6);
-rtl.z = preprocess("/Volumes/clmtlbry-4/UCT/IV/S/EEE4022S/Data/DAB_data/RTL-SDR/DAB.bin", "short", 2, 0, 0, mode1, 2.048e6);
-
-perfect.name = "Report/Images/plots/dqpsk_demap_perfect";
-raw.name =  "Report/Images/plots/dqpsk_demap_raw";
-rtl.name =  "Report/Images/plots/dqpsk_demap_rtl";
-
+% Choose which file to use
 file = rtl;
 
-s = symbols_unpack(file.z, mode1);
-c = ofdm_demux(s);
-d = dqpsk_demap(c, mode1);
-d_deint = freq_deinterleave(d, build_interleave_map());
-
-d_snap = dqpsk_snap(d_deint);
-
+% Plot colour
 colour = [0, 0.4470, 0.7410];
 
+% --
+
+% Symbols
+s = symbols_unpack(file.z, dab_mode);
+
+% Carriers
+c = ofdm_demux(s);
+
+% DQPSK data, unsnapped
+d = dqpsk_demap(c, dab_mode);
+
+% DQPSK data, deinterleaved, unsnapped
+d_deint = freq_deinterleave(d, build_interleave_map());
+
+% SNAPPED
+d_snap = dqpsk_snap(d_deint);
+
+% Plot data before and after snapping
 figure(1);
 hold off;
 plot(d_deint(2,:), 'bo', 'MarkerFaceColor', colour, 'MarkerSize', 7);
 hold on;
 plot(d_snap(2,:),'rx', 'LineWidth', 4, 'MarkerSize',20);
 
+% Draw lines to show snapping points
 x = 10/sqrt(2);
 plot([0 x],[0 x],'--r');
 plot([0 x],[0 -x],'--r');
@@ -44,6 +50,7 @@ xline(0,'k',"Im");
 yline(0,'k',"Re");
 axis([-1.2 1.2 -1.2 1.2]);
 
+% Plot data across carrier indices
 figure(2);
 hold off;
 plot(angle(d_deint(2,:)), 'o', 'MarkerFaceColor', colour, 'MarkerEdgeColor', colour);
@@ -56,9 +63,4 @@ yline(      0,'k');
 yline(-  pi/2,'k');
 yline(-    pi,'k');
 
-figure(3);
-plot(d_snap(2,47:50),'o', 'MarkerFaceColor', colour, 'MarkerSize', 8);
-grid on;
-xline(0,'k',"Im");
-yline(0,'k',"Re");
-axis([-1 1 -1 1]);
+% --
